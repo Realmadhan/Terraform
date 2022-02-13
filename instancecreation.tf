d784f2965b82d908510a76e222dfd70e69d32d94
Madhan <96423587+Realmadhan@users.noreply.github.com>
2022-02-13 18:39:46 +0530

    Update instancecreation.tf

--- instancecreation.tf
+++ instancecreation.tf
@@ -1,10 +1,10 @@
 provider "aws"{
-  region = us-east-1 
+  region = "us-east-1" 
 }
 resource "aws_instance" "terraform_ec2"{
-  ami = "04505e74c0741db8d"
+  ami = "ami-04505e74c0741db8d"
   instance_type = "t2.micro"
-  key-name = "Linkey"
+  key_name = "Linkey"
   tags = {
     Name = "Ubuntu Server"
   }
