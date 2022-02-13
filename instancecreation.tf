5738531d7cdab9e79e05c43293decacf5784deef
Madhan <96423587+Realmadhan@users.noreply.github.com>
2022-02-13 18:10:20 +0530

    Create instancecreation.tf

---
+++ instancecreation.tf
@@ -0,0 +1,10 @@
+provider "aws"{
+  region = us-east-1 
+}
+resource "aws_instance" "terraform_ec2"{
+  ami = "04505e74c0741db8d"
+  instance_type = "t2.micro"
+  key-name = "Linkey"
+  tags = {
+    Name = "Ubuntu Server"
+  }
